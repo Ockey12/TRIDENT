a182b797bd51b8b8708d025af2e27efd26ac8ef0
Ockey12 <haloupgarou@gmail.com>
2025-05-11 21:07:45 +0900

    create project

---
+++ TRIDENT/TRIDENT/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TRIDENT
+//
+//  Created by Ockey on 2025/05/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TRIDENT/TRIDENT/TRIDENTApp.swift
@@ -0,0 +1,17 @@
+//
+//  TRIDENTApp.swift
+//  TRIDENT
+//
+//  Created by Ockey on 2025/05/11.
+//
+
+import SwiftUI
+
+@main
+struct TRIDENTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
